7546e401ca86e81d14ec474074efaf86ed93068a
Steve Toro <steve@stevetoro.com>
2018-07-22 17:39:19 -0400

    PUT /employees/:employeeId

--- api/employees.js
+++ api/employees.js
@@ -47,4 +47,30 @@ router.get('/:employeeId', (req, res, next) => {
   return res.status(200).send({ employee: req.employee });
 });
 
+router.put('/:employeeId', (req, res, next) => {
+  const { name, position, wage, is_currently_employed = 1 } = req.body.employee;
+  if (!(name && position && wage)) return res.sendStatus(400);
+
+  db.run(`
+    UPDATE Employee SET name = $name,
+      position = $position,
+      wage = $wage,
+      is_currently_employed = $is_currently_employed
+    WHERE Employee.id = ${req.params.employeeId}`,
+    {
+      $name: name,
+      $position: position,
+      $wage: wage,
+      $is_currently_employed: is_currently_employed,
+    },
+    function (err) {
+      if (err) return next(err);
+      db.get(`SELECT * FROM Employee WHERE Employee.id = ${req.params.employeeId}`, (err, employee) => {
+        if (err) return next(err);
+        return res.status(200).send({ employee });
+      }); 
+    }
+  );
+});
+
 module.exports = router;
